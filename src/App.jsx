5f9f4f354112bbb5bbf3a8deecb9638e14689626
MuhammadYahyo2112 <yahyo0121@gmail.com>
2025-09-17 14:18:15 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Buks from './components/Buks'
+import Books2 from './components/Books2'
+
+const App = () => {
+  return (
+    <div>
+      <Buks/>
+      <Books2/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Buks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Buks = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Buks
